751dbc3c5fab68305b64e8db87836456a8a4b615
Annette <Annette@cu-biot-6-10.203.151.21.int.colorado.edu>
2024-04-17 01:08:19 -0600

    Template for enzyme balance codes

--- Matlab/P. putida/troubleshoot.m
+++ Matlab/P. putida/troubleshoot.m
@@ -1,19 +1,47 @@
-C=Cd2;
-T=Td2;
+%% Balance
+% Need to have ran the beginning of the real code
 
-F_weighted = zeros(length(T),1);
-F_saved = zeros(1,length(S.FA_dist));
-F_raw = zeros(1,length(S.FA_dist));
-weight_vec = S.FA_dist/16; %Palmitic Acid equivalents (C16)
-count = 1;
-for ind = 1:length(S.labels)
-    label_val = char(S.labels(ind));
-    if contains(label_val, '_FA','IgnoreCase',false)
-        F_saved(count) = weight_vec(count)*(C(end,ind));
-        F_raw(count) = C(end,ind);
-        F_weighted = weight_vec(count)*(C(:,ind)) + F_weighted;
-        count = count + 1;
-    end
+S.labels = {};
+
+S.range = [0 150]; %2.5 mins (initial rate)
+
+S.num = length(S.labels); %how many diff eqs there are
+
+% New order from var_name code
+S.init_cond = zeros(S.num,1);
+S.init_cond(1) = 100;
+
+% (ACC,FabD,FabH,FabG,FabZ,FabI,TesA,FabF,FabA,FabB)
+enz_conc = [0 0 0 0 0 0 0 0 0 0]; 
+
+S.enzyme_conc = enz_conc;
+
+P = Param_Function(S);
+
+parameterized_ODEs = @(t,c) ODE_Function_troubleshoot(t,c,P);
+tic
+[T,C] = ode15s(parameterized_ODEs,S.range,S.init_cond,ODE_options);
+toc
+[~, rel_rate] = Calc_Function(T,C,S);
+
+[balance_conc, balances, total_conc, carbon] = mass_balance(C,P);
+
+
+function dcdt = ODE_Function_troubleshoot(t,c,P)
+
+% Assign values to each variable
+for var = 1:numel(P.labels)
+    % Get the variable name
+    var_name = P.labels{var};
+    
+    % Assign the value to the variable in the workspace
+    eval([var_name ' = c(var, :);']);
 end
 
-rel_rate = F_weighted(end,1)/150*60;
+
+% Set of differential equations
+
+
+dcdt = [];
+
+end
